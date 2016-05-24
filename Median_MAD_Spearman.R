672b72409f6ce3b49741507728f6c6b377ace1cd
Rahul Madhavan <rahul.maddy@gmail.com>
2016-05-24 10:00:03 +0530

    small changes

--- Median_MAD_Spearman.R
+++ Median_MAD_Spearman.R
@@ -40,9 +40,37 @@ plot(chick$weight.21,chick$weight.4,col=chick$Diet)
 a <- cor(chick$weight.21,chick$weight.4,method="pearson")
 b <- cor(c(3000,chick$weight.21),c(3000,chick$weight.4),method="pearson")
 b/a
+
+
+
+
 a1 <- cor(chick$weight.21,chick$weight.4,method="spearman")
 b1 <- cor(c(3000,chick$weight.21),c(3000,chick$weight.4),method="spearman")
 b1/a1
+
+# some more exercises on wilcoxon statistic
+x = chick[chick$Diet==1,"weight.4"]
+y = chick[chick$Diet==4,"weight.4"]
+t.test(x,y)
+wilcox.test(x,y)
+t.test(c(x,200),y)$p.value
+
+
+#disadvantages of wilcox test
+library(rafalib)
+mypar(1,3)
+boxplot(x,y)
+boxplot(x,y+10)
+boxplot(x,y+100)
+
+t.test(x,y+10)$statistic-t.test(x,y+100)$statistic
+wilcox.test(x,y+10)$statistic-wilcox.test(x,y+100)$statistic #0
+
+wilcox.test(c(1,2,3),c(4,5,6)) 
+# above is high p value of 0.1 compared to 0.02 for tstat and cor =1
+t.test(c(1,2,3),c(4,5,6))$p.value # 0.0213
+wilcox.test(c(1,2,3),c(400,500,600))$p.value # still at 0.1
+t.test(c(1,2,3),c(400,500,600))$p.value #0.01316887
 
 #logratios
 x <- 2^(rnorm(100))
@@ -90,3 +118,4 @@ print(Z)
 
 # In other words:
 # Z-score = ((U/n1)-(n2/2))/sqrt(n2*(n1+n2+1)/(12*n1))
+
